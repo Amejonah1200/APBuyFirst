72d2cb3f9e8acdf4cba719994351a1348da2d3e5
Amejonah1200 <amejonah@gmail.com>
2019-08-05 16:39:30 +0200

    Update APBuy.java

--- src/ap/apb/APBuy.java
+++ src/ap/apb/APBuy.java
@@ -1,3 +1,24 @@
+/**
+ * @author Amejonah1200
+ *
+ *         APBuy: Better than a marketplace plugin.
+ * 
+ *         Copyright (C) 2019 Amejonah1200
+ *         This program is free software: you can redistribute it and/or modify
+ *         it under the terms of the GNU Affero General Public License as
+ *         published
+ *         by the Free Software Foundation, either version 3 of the License, or
+ *         (at your option) any later version.
+ * 
+ *         This program is distributed in the hope that it will be useful,
+ *         but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *         GNU Affero General Public License for more details.
+ * 
+ *         You should have received a copy of the GNU Affero General Public
+ *         License
+ *         along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package ap.apb;
 
 import java.io.File;
@@ -144,7 +165,7 @@ public class APBuy extends JavaPlugin {
 		// icatslist.add(icats.new ICatFood());
 		// icatslist.add(icats.new ICatBrewing());
 		// icatslist.add(icats.new ICatRedstone());
-		// ItoomelCat icat = new ItoomelCat("ง7Other stuff", Material.CHEST,
+		// ItoomelCat icat = new ItoomelCat("ยง7Other stuff", Material.CHEST,
 		// "Hier wird alles sein was nicht einsortiert werden konnte.") {
 		// @Override
 		// public ItoomelCat registerMats() {
